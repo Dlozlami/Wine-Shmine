33a508f08bae2bf73da1bd6db525a0899430060c
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-21 10:33:44 +0200

    navi container

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,76 +1,15 @@
-import React from 'react';
-import { Tabs } from 'expo-router/tabs';
-import { MaterialIcons } from '@expo/vector-icons';
-import { View, Image } from 'react-native';
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './home';
+import CartScreen from './cart'
 
-export default function AppLayout() {
-  const renderProfileIcon = () => {
-    // Replace 'user_profile_pic.png' with the correct path to the user's profile picture.
-    return (
-      <View style={{ marginRight: 10 }}>
-        <Image
-          source={require('../../assets/users/uxdlozi.jpg')}
-          style={{ width: 30, height: 30, borderRadius: 15 }}
-        />
-      </View>
-    );
-  };
+const Stack = createNativeStackNavigator();
 
+export default  function Root() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel: false, // Hide tab labels
-        activeTintColor: '#d2ff58',
-      }}
-    >
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: 'Home | Wine Shmine',
-          headerTitle:'',
-          headerRight: renderProfileIcon,
-          headerStyle: {backgroundColor: 'rgba(255, 255, 255, 0.0)'},
-          headerShadowVisible:false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <MaterialIcons name="home" style={{ color: focused ? '#08341d' : color }} size={size} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="cart"
-        options={{
-          title: 'Cart | Wine Shmine',
-          headerTitle:'',
-          headerRight: renderProfileIcon,
-          headerStyle: {backgroundColor: 'rgba(255, 255, 255, 0.0)'},
-          headerShadowVisible:false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <MaterialIcons
-              name="shopping-cart"
-              style={{ color: focused ? '#08341d' : color }}
-              size={size}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="contact"
-        options={{
-          title: 'Contact us |  Wine Shmine',
-          headerTitle:'',
-          headerRight: renderProfileIcon,
-          headerStyle: {backgroundColor: 'rgba(255, 255, 255, 0.0)'},
-          headerShadowVisible:false,
-          headerShadowVisible:false,
-          tabBarIcon: ({ focused, color, size }) => (
-            <MaterialIcons
-              name="contact-support"
-              style={{ color: focused ? '#08341d' : color }}
-              size={size}
-            />
-          ),
-        }}
-      />
-    </Tabs>
+      <Stack.Navigator>
+      <Stack.Screen name="home" component={HomeScreen} options={{ title: 'Home' }}/>
+      <Stack.Screen name="cart" component={CartScreen} options={{ title: 'Cart' }}/>
+      </Stack.Navigator>
   );
 }
--- app/login.js
+++ app/login.js
@@ -4,11 +4,7 @@ import { Image, Text, View } from 'react-native';
 export default function Login() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Stack.Screen
-        options={{
-          title: 'Login | Wine Shmine',
-        }}
-      />
+
 
     </View>
   );
