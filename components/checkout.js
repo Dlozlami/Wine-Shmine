754de66337c1838a34256cea609fe6c88ee951ca
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-24 17:17:00 +0200

    check

--- components/checkout.js
+++ components/checkout.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import  { Paystack }  from 'react-native-paystack-webview';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { useNavigation } from '@react-navigation/native';
+import {clearCheckoutData,clearitemsList} from '../feature/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function Pay() {
+export default function Checkout({setModalOpen}) {
+  const dispatch = useDispatch();
+    const navigation = useNavigation();
+    const authorization_url =  useSelector((state) => state.cart.checkoutData.authorization_url);
+    const callback_url = useSelector((state) => state.cart.callBackURL);
+
+  onNavigationStateChange = state => {
+ 
+    const { url } = state;
+
+    if (!url) return;
+
+    if (url === callback_url) {
+			// get transaction reference from url and verify transaction, then redirect
+      const redirectTo = 'window.location = "' + callback_url + '"';
+      this.webview.injectJavaScript(redirectTo);
+    }
+		
+		if(url === 'https://standard.paystack.co/close') {
+      // handle webview removal
+      // You can either unmount the component, or
+      // Use a navigator to pop off the view
+      //dispatch(clearCheckoutData);
+      //dispatch(clearitemsList);
+      setModalOpen(false);
+    }
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <Paystack  
-        paystackKey="pk_test_24136f8ff804d753571a93efdac04e117f057dee"
-        amount={250000}
-        billingEmail="paystackwebview@something.com"
-        activityIndicatorColor="green"
-        onCancel={(e) => {
-          // handle response here
-        }}
-        onSuccess={(res) => {
-          // handle response here
-        }}
-        autoStart={true}
-      />
-    </View>
+    <WebView 
+      source={{ uri: authorization_url }}
+      style={{ marginTop: 40 }}
+      onNavigationStateChange={ this.onNavigationStateChange }
+    />
   );
 }
