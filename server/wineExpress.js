6b0b53cc0678a8619461a283c28b4b4162b99ad9
Dlozi Mthethwa <dlozi.mthethwa@gmail.com>
2023-07-24 07:57:11 +0200

    payment gateway

--- server/wineExpress.js
+++ server/wineExpress.js
@@ -15,7 +15,53 @@ const refreshKey = 'o8uhs7errqlosw830j#^*)N[]xsyg26gednpsrtuui98908732fros-w;w.s
 mongoose.connect('mongodb://127.0.0.1:27017/wine-list')
 
 
-
+app.post('/make-payment', (req, res) => {
+	// Extract the required data from the request body
+	const { email, amount } = req.body;
+  
+	// Construct the params object from the extracted data
+	const params = JSON.stringify({
+	  "email": email,
+	  "amount": amount
+	});
+  
+	// Set up the options for the HTTPS request
+	const options = {
+	  hostname: 'api.paystack.co',
+	  port: 443,
+	  path: '/transaction/initialize',
+	  method: 'POST',
+	  headers: {
+		Authorization: 'Bearer SECRET_KEY', // Replace with your actual Paystack API secret key
+		'Content-Type': 'application/json'
+	  }
+	};
+  
+	// Make the HTTPS request to the Paystack API
+	const reqPaystack = https.request(options, response => {
+	  let data = '';
+  
+	  response.on('data', chunk => {
+		data += chunk;
+	  });
+  
+	  response.on('end', () => {
+		// Parse the response data and send it back to the client
+		const responseData = JSON.parse(data);
+		res.json(responseData);
+	  });
+	});
+  
+	reqPaystack.on('error', error => {
+	  console.error(error);
+	  res.status(500).json({ error: 'An error occurred while processing the payment.' });
+	});
+  
+	// Write the request body (params) and end the request
+	reqPaystack.write(params);
+	reqPaystack.end();
+  });
+  
 
 
 app.listen(8080, function () {
